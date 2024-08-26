f2930482ee73128a929ddc37c3b513cf3f5221f2
akisliy <oleksandr.kyslyi@onix-systems.com>
2024-08-26 16:01:42 +0300

    innit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import { StreamChat } from 'stream-chat';
 import CustomChat from "@/components/Chat/Chat";
 
 export default function Home() {
+  const api_key = process.env.STREAM_KEY;
+  const api_secret = process.env.STREAM_SECRET;
+  const user_id = process.env.STREAM_USER_ID;
+
+  const serverClient = StreamChat.getInstance(api_key, api_secret);
+  const token = serverClient.createToken(user_id);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CustomChat />
+      <CustomChat
+        apiKey={process.env.STREAM_KEY}
+        userId={process.env.STREAM_USER_ID}
+        token={token}
+      />
     </main>
   );
 }
--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -11,33 +11,26 @@ import {
   Thread,
   useCreateChatClient,
 } from 'stream-chat-react';
+import CreateChannel from "@/components/CreateChannel/CreateChannel";
 import 'stream-chat-react/dist/css/v2/index.css';
 
-const apiKey = 'r6578ydqv5ud';
-const userId = 'test';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidGVzdCJ9.QqC00TkTnfTQDjf-h6WmsQ9-qgJt2xJuT1Acodh5Fw8';
-
-const filters = { members: { $in: [userId] }, type: 'messaging' };
-const options = { presence: true, state: true };
-const sort = { last_message_at: -1 };
-
-const CustomChat = () => {
+const CustomChat = ({ apiKey, token, userId }) => {
   const client = useCreateChatClient({
     apiKey,
     tokenOrProvider: token,
-    userData: { id: userId },
+    userData: { id: userId, name: 'John test' },
   });
-  
+
   if (!client) return <div>Loading...</div>;
-  
-  const channel = client.channel('messaging', 'travel', {
-    name: 'Awesome channel about traveling',
-  });
+
+  const filters = { members: { $in: [userId] }, type: 'messaging' };
+  const options = { presence: true, state: true };
 
   return (
     <Chat client={client}>
-      <ChannelList sort={sort} filters={filters} options={options} />
-      <Channel channel={channel}>
+      <ChannelList filters={filters} options={options} />
+      <CreateChannel onClose={() => {}} toggleMobile={() => {}} />
+      <Channel>
         <Window>
           <ChannelHeader />
           <MessageList />

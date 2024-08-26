1d4da4a2dd2d7f82991ae95517917975ce2c3002
akisliy <oleksandr.kyslyi@onix-systems.com>
2024-08-26 16:17:28 +0300

    innit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { StreamChat } from 'stream-chat';
+import { username } from 'username';
 import CustomChat from "@/components/Chat/Chat";
 
-export default function Home() {
+export default async function Home() {
   const api_key = process.env.STREAM_KEY;
   const api_secret = process.env.STREAM_SECRET;
-  const user_id = process.env.STREAM_USER_ID;
+  const user_id = await username();
 
   const serverClient = StreamChat.getInstance(api_key, api_secret);
   const token = serverClient.createToken(user_id);
@@ -13,7 +14,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <CustomChat
         apiKey={process.env.STREAM_KEY}
-        userId={process.env.STREAM_USER_ID}
+        userId={user_id}
         token={token}
       />
     </main>

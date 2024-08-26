a8e0420ad1f76f97627a106760a2bfecabd6ff03
akisliy <oleksandr.kyslyi@onix-systems.com>
2024-08-26 13:06:54 +0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import CustomChat from "@/components/Chat/Chat";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <CustomChat />
+    </main>
+  );
+}
